a670ba7a54c2a1f75c35aa15ac0f330ca2c3bf64
Erwin Bonsma <erwin.bonsma@topic.nl>
2025-03-21 16:38:03 +0100

    Attempt to solve Problem 1 - Part 2

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -1,5 +1,7 @@
 use regex::Regex;
 use std::error::Error;
+use std::collections::HashMap;
+use itertools::Itertools;
 
 fn find_number(s: &str, regex: &Regex) -> u32 {
     let (_, [digit]) = regex.captures(s)
@@ -9,9 +11,7 @@ fn find_number(s: &str, regex: &Regex) -> u32 {
     digit.parse::<u32>().unwrap()
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let input = include_str!("input.txt");
-
+fn part1(input: &str) -> Result<(), Box<dyn Error>> {
     let regex_first = Regex::new(r"^\D*(\d)")?;
     let regex_last = Regex::new(r"(\d)\D*$")?;
 
@@ -23,4 +23,53 @@ fn main() -> Result<(), Box<dyn Error>> {
     println!("{}", result);
 
     Ok(())
+}
+
+fn value_of(digit: &str, digits: &HashMap<&str, i32>) -> i32 {
+    if let Some(&value) = digits.get(digit) {
+        value
+    } else {
+        digit.parse::<i32>().unwrap()
+    }
+}
+
+fn part2(input: &str) -> Result<(), Box<dyn Error>> {
+    let digits = HashMap::from([
+        ("one", 1),
+        ("two", 2), 
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9)
+    ]);
+
+    let pattern = digits.keys().map(|&key| key).join("|");
+    let regex = Regex::new(&(r"\d|".to_owned() + &pattern))?;
+    let mut total = 0;
+    for line in input.lines() {
+        let matches: Vec<_> = regex.find_iter(line).map(|m| m.as_str()).collect();
+        let value =
+            value_of(matches[0], &digits) * 10
+            + value_of(matches.last().unwrap(), &digits);
+        println!("{} {:?} {}", line, matches, value);
+        total += value;
+    }
+
+    println!("{}", total);
+
+    Ok(())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let input = include_str!("input.txt");
+
+    part1(input)?;
+
+    part2(input)
+    // Wrong: 54970
+    // Problem: It does not correctly handle overlapping matches. In particular, when it
+    // encounters "oneight", it will only return "one" as a match, not "eight".
 }
