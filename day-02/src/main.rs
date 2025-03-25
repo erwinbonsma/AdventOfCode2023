43008a638d61cd36fb40153d01e0a734eee35b8e
Erwin Bonsma <erwin.bonsma@topic.nl>
2025-03-25 17:00:57 +0100

    Solve Day 2 - Part 2

--- day-02/src/main.rs
+++ day-02/src/main.rs
@@ -1,4 +1,6 @@
 use std::error::Error;
+use std::collections::HashMap;
+use std::cmp::max;
 
 fn part1(input: &str) -> Result<(), Box<dyn Error>> {
     let mut total = 0;
@@ -53,8 +55,42 @@ fn part1(input: &str) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+fn part2(input: &str) -> Result<(), Box<dyn Error>> {
+    let mut total = 0;
+
+    for line in input.lines() {
+        let colon_pos = line.find(':').unwrap();
+        let results = line[colon_pos + 2..].split("; ").collect::<Vec<&str>>();
+
+        let mut min_cubes = HashMap::new();
+        for result in results {
+            let items = result.split(", ").collect::<Vec<&str>>();
+            for item in items {
+                let fields = item.split(" ").collect::<Vec<&str>>();
+                let num = fields[0].parse::<u32>().unwrap();
+                let color = fields[1];
+                let count = min_cubes.entry(color).or_insert(0);
+                *count = max(*count, num);
+            }
+        }
+
+        if min_cubes.len() == 3 {
+            let prod = min_cubes.iter().fold(1, |acc, (_, &value)| acc * value);
+            total += prod;
+        } else {
+            println!("One color missing: {} => {:?}", line, min_cubes);
+        }
+    }
+
+    println!("{}", total);
+
+    Ok(())
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
     let input = include_str!("input.txt");
 
-    part1(input)
+    part1(input)?;
+
+    part2(input)
 }
